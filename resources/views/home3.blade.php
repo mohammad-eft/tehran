4f50a92d846f791fb0f1d13cfc4730b7cb2e2270
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-08-24 16:56:37 +0330

    4 version of home

---
+++ resources/views/home3.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('assets/js/tailwind.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="w-full h-dvh flex justify-center items-center">
+    
+    <div class="w-9/12 h-full">
+        <img src="{{ assetO('assets/img/mobios1.jpg') }}" alt="" class="w-full h-full">
+    </div>
+    <div class="w-3/12 h-full">
+        <img src="{{ asset('assets/img/ayz.jpg') }}" alt="" class="w-full h-full">
+        
+    </div>
+
+</header>
+</body>
+</html>
